1c02c895d957cda7660f44a991b53244e82d809a
yonk <nopctoday@gmail.com>
2019-09-11 14:43:11 +0200

    Modified converter so it can adapt non square terrain (Slovenia the whole country doesn't fit in a square :P)

--- utils/eo_converter.py
+++ utils/eo_converter.py
@@ -21,7 +21,7 @@ PATCHES_H = 10
 def main():
     # Read through all directories
     with h5py.File(DATASET_DIR + "slovenia2017.hdf5", "w") as f:
-        h5set = f
+
         n_patches = len(os.listdir(DATASET_DIR))
         for i, patch_name in enumerate(os.listdir(DATASET_DIR)):
             printProgressBar(i, n_patches)
@@ -31,71 +31,66 @@ def main():
 
             eo_patch = EOPatch.load(patch_path)
 
-            patch_indices = extract_name(patch_name)
-
+            h5set = f.create_group(patch_name)
             # Write raw bands data (images)
             data = eo_patch.data['BANDS']
-            subset = write_to_hdf5(h5set, 'data_bands', patch_indices, data=data)
+            subset = write_to_hdf5(h5set, 'data_bands', data=data, is_timeseries=True)
 
             # Write mask
             data = eo_patch.mask['CLM']
-            subset = write_to_hdf5(h5set, 'mask/clm', patch_indices, data=data)
+            subset = write_to_hdf5(h5set, 'mask/clm',  data=data, is_timeseries=True)
 
             data = eo_patch.mask['IS_DATA']
-            subset = write_to_hdf5(h5set, 'mask/is_data', patch_indices, data=data)
+            subset = write_to_hdf5(h5set, 'mask/is_data', data=data, is_timeseries=True)
 
             data = eo_patch.mask['VALID_DATA']
-            subset = write_to_hdf5(h5set, 'mask/valid_data', patch_indices, data=data)
+            subset = write_to_hdf5(h5set, 'mask/valid_data', data=data, is_timeseries=True)
 
             # Write timeless mask
             data = eo_patch.mask_timeless['LULC']
-            subset = write_to_hdf5(h5set, 'mask_timeless/lulc', patch_indices, data=data)
+            subset = write_to_hdf5(h5set, 'mask_timeless/lulc', data=data)
 
             data = eo_patch.mask_timeless['VALID_COUNT']
-            subset = write_to_hdf5(h5set, 'mask_timeless/valid_count', patch_indices, data=data)
+            subset = write_to_hdf5(h5set, 'mask_timeless/valid_count', data=data)
 
             # Write time stamp (convert to unix timestamp UTC because h5py doesn't support time)
             datetimestamps = eo_patch.timestamp
             timestamps_unix_epoch = [ dt.timestamp() for dt in datetimestamps]
             data = np.array(timestamps_unix_epoch)
-            subset = write_to_hdf5(h5set, 'timestamp', patch_indices, data=data)
+            subset = write_to_hdf5(h5set, 'timestamp', data=data)
 
             # Bounding box
             bbox = eo_patch.bbox
             # TODO Double check if grid is y axis major or x axis major
             data = np.array([bbox.min_x, bbox.min_y, bbox.max_x, bbox.max_y])
-            write_to_hdf5(h5set, 'bbox', patch_indices, data=data)
+            write_to_hdf5(h5set, 'bbox', data=data)
 
             # Metadata
             metadict = dict(eo_patch.meta_info)
             data = np.void(pickle.dumps(metadict))
-            write_to_hdf5(h5set, 'pickled_metadata', patch_indices, data=data)
+            write_to_hdf5(h5set, 'pickled_metadata', data=data)
+
+
+
+def write_to_hdf5(dset,
+                  subset_name,
+                  data,
+                is_timeseries=False,
+                  ):
+    dtype = data.dtype
+    # Chunk data by time step if it's time series
+    chunk_shape = (1,) + data.shape[1:] if is_timeseries else None
+    subset = dset.create_dataset(subset_name,
+                                 dtype = dtype,
+                                 chunks = chunk_shape,
+                                 data = data,
+                             )
+    return subset
 
 def extract_name(eopatch_name):
     indices = tuple(int(s) for s in re.findall(r'\d+', eopatch_name))
     assert len(indices) == 2, 'Invalid eopatch file name'
     return indices
-
-def write_to_hdf5(dset,
-                  subset_name,
-                  index,
-                  data):
-
-    # Create dataset if it doesn't exists
-    if subset_name not in dset.keys():
-        shape = (PATCHES_W, PATCHES_H) + data.shape
-        chunk_shape = (1, 1) + data.shape
-        dtype = data.dtype
-        subset = dset.create_dataset(subset_name,
-                                 dtype=dtype,
-                                 shape=shape,
-                                 chunks=chunk_shape,  # 10 images per chunk
-                                 )
-    else:
-        subset = dset[subset_name]
-
-    subset[index] = data
-    return subset
 
 def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█'):
     """
