f56d851d55d199afd2d26147e8cffc6476a7da41
yonk <nopctoday@gmail.com>
2019-09-17 10:15:23 +0200

    Changing network channels

--- train.py
+++ train.py
@@ -87,7 +87,7 @@ def get_args():
 if __name__ == '__main__':
     args = get_args()
 
-    net = UNet(n_channels=8, n_classes=9)
+    net = UNet(n_channels=6, n_classes=10)
 
     if args.load:
         net.load_state_dict(torch.load(args.load))
