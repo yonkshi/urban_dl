ff580bcd34431726a99852c42f05aa62541b6619
yonk <nopctoday@gmail.com>
2019-09-27 18:10:07 +0200

    shape fix

--- unet/utils/load.py
+++ unet/utils/load.py
@@ -34,7 +34,8 @@ class SloveniaDataset(torch.utils.data.Dataset):
         # sometimes data can exceed [0, 1], clip em!
         obs = np.clip(obs, 0,1)
 
-        cloud_mask = subset['mask/valid_data'][timeidx].squeeze(-1).astype(np.float32)
+        cloud_mask = subset['mask/valid_data'][timeidx].astype(np.float32)
+        cloud_mask = np.moveaxis(cloud_mask, -1, 0)
 
         # TODO For now, only pick the first image of each pixel
 
