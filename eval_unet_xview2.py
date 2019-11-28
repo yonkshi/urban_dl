2adb701fa16ab8668ad2411d805309aba1a0724c
yonk <nopctoday@gmail.com>
2019-11-28 16:29:36 +0100

    auto-sync

--- eval_unet_xview2.py
+++ eval_unet_xview2.py
@@ -251,6 +251,8 @@ def list_and_sort_checkpoint_files():
     Iterate through the designated log directory and load all
     :return:
     '''
+    print('entering files', cfg.OUTPUT_DIR)
+
     file_list = [(int(f.split('_')[1].split('.')[0]),f)  # e.g. (123, 'cp_123.pkl')
                  for f in listdir(cfg.OUTPUT_DIR)
                  if isfile(join(cfg.OUTPUT_DIR, f))
