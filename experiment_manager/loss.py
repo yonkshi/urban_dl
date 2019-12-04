021427fb48486ad605de85cf77828f54e2bf2c8f
yonk <nopctoday@gmail.com>
2019-12-04 18:47:26 +0100

    auto-sync

--- experiment_manager/loss.py
+++ experiment_manager/loss.py
@@ -1,8 +1,9 @@
 import torch
 def soft_dice_loss(input:torch.Tensor, target:torch.Tensor):
+    input_sigmoid = torch.sigmoid(input)
     smooth = 1.
 
-    iflat = input.flatten()
+    iflat = input_sigmoid.flatten()
     tflat = target.flatten()
     intersection = (iflat * tflat).sum()
 
