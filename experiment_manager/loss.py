8ab63a6654f1ff79c2e3a14e671035e3952425fe
yonk <nopctoday@gmail.com>
2019-12-05 09:06:53 +0100

    auto-sync

--- experiment_manager/loss.py
+++ experiment_manager/loss.py
@@ -1,11 +1,11 @@
 import torch
 def soft_dice_loss(input:torch.Tensor, target:torch.Tensor):
     input_sigmoid = torch.sigmoid(input)
-    smooth = 1.
+    eps = 1e-6
 
     iflat = input_sigmoid.flatten()
     tflat = target.flatten()
     intersection = (iflat * tflat).sum()
 
-    return 1 - ((2. * intersection + smooth) /
-                (iflat.sum() + tflat.sum() + smooth))
+    return 1 - ((2. * intersection) /
+                (iflat.sum() + tflat.sum() + eps))
