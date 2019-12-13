8b35a05bf1c585cc17d8935da074d26767192a32
yonk <nopctoday@gmail.com>
2019-12-13 16:35:17 +0100

    auto-sync

--- unet/augmentations.py
+++ unet/augmentations.py
@@ -19,7 +19,8 @@ class Resize():
 class PIL2Torch():
     def __call__(self, args):
         input, label = args
-        input = input[..., [2,0,1]]
+        # BGR to RGB
+        input = input[..., [2,1,0]]
         input_t = TF.to_tensor(input)
         return input_t, label
 
