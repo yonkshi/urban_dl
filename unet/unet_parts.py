8dd88cc3de4ca07082505ee48836140ccc7a06e5
yonk <nopctoday@gmail.com>
2019-12-16 15:29:48 +0100

    auto-sync

--- unet/unet_parts.py
+++ unet/unet_parts.py
@@ -33,7 +33,7 @@ class triple_conv(nn.Module):
             nn.Conv2d(out_ch, out_ch, 3, padding=1),
             nn.BatchNorm2d(out_ch),
             nn.ReLU(inplace=True),
-            nn.Conv2d(in_ch, out_ch, 3, padding=1),
+            nn.Conv2d(out_ch, out_ch, 3, padding=1),
             nn.BatchNorm2d(out_ch),
             nn.ReLU(inplace=True),
         )
