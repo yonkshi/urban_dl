b1eba3ac6184869d80e5d0b226c1507a93467dac
yonk <nopctoday@gmail.com>
2020-02-28 13:55:16 +0100

    auto-sync

--- eval_util/ablation_test.py
+++ eval_util/ablation_test.py
@@ -126,7 +126,7 @@ def compute_sample(x, Y_true, Y_pred, img_filenames, indices):
                                                  mode='bilinear')
     # expand batch
     Y_pred = Y_pred.squeeze(1) >= THRESHOLD  # remove empty channel and activate Y_pred
-    Y_true = Y_true.type(torch.bool)
+    Y_true = Y_true.squeeze(1).type(torch.bool)
 
     hw_dims = (-1, -2)
     # Compute all TP, TN, FP, FP, FN at once
